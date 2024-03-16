2d3a20731eaa160413429772363a4e7e8f367947
dayanjr <dayanfrazao@byui.edu>
2024-03-15 19:57:29 -0600

    up

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,18 @@ const router = express.Router();
 //TODO: Uncomment Route as you finish the Controller for your Controller
 
 //Routes for recipes
-router.use('/recipes', require('./recipeRoutes'));
-
-//routes for users
-router.use('/users', require('./userRoutes'));
-
-//routes for ingredients
-router.use('/ingredients', require('./ingredientRoute'));
-
-//Routes for posts
-router.use('/posts', require('./postsRoutes'));
-
-//swagger
+//router.use('/recipes', require('./recipeRoutes'));
+//
+////routes for users
+//router.use('/users', require('./userRoutes'));
+//
+////routes for ingredients
+//router.use('/ingredients', require('./ingredientRoute'));
+//
+////Routes for posts
+//router.use('/posts', require('./postsRoutes'));
+//
+////swagger
 router.use('/', require('./swagger'));
 
 module.exports = router;
