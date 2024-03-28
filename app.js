c0320eaa62bc849c5228d68e7a502136c1748d1d
kerokero014 <kevinjrm.kr@gmail.com>
2024-03-28 14:51:40 -0600

    Commented out Auth0 setup cod

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const routes = require('./routes');
 const cors = require('cors');
 const { auth } = require('express-oauth2-jwt-bearer');
 
-// Auth0 setup
-const jwtCheck = auth({
-  audience: 'https://CookingRecipe-api.com',
-  issuerBaseURL: 'https://dev-sa6dftpsfnputuuv.us.auth0.com/',
-  tokenSigningAlg: 'RS256'
-});
+//// Auth0 setup
+//const jwtCheck = auth({
+//  audience: 'https://CookingRecipe-api.com',
+//  issuerBaseURL: 'https://dev-sa6dftpsfnputuuv.us.auth0.com/',
+//  tokenSigningAlg: 'RS256'
+//});
 
 // MongoDB connection
 mongoose
@@ -28,11 +28,11 @@ mongoose
     console.error(`DB Connection Error: ${err.message}`);
   });
 
-  
 app.use(express.json());
 app.use(cors());
 
-app.use(jwtCheck);
+//app.use(jwtCheck);
+
 // Routes
 app.use('/', routes);
 
