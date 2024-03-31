0c75a09bc81b1f1ed9944a030f0cb06731856cc9
dayanjr <dayanfrazao@byui.edu>
2024-03-30 18:30:15 -0600

    up

--- schemas/commentsSchema.js
+++ schemas/commentsSchema.js
@@ -6,14 +6,15 @@ const CommentsSchema = new Schema(
   {
     name: {
       type: String,
-      required: true,
       required: [true, 'Name is required'],
       trim: true,
       maxlength: [200, 'Name cannot be more than 200 characters'] // Maximum 200 characters
     },
     comment: {
       type: String,
-      required: true
+      required: [true, 'Comment is required'],
+      maxlength: [2000, 'Name cannot be more than 200 characters'], // Maximum 2000 characters,
+      minlength: [2, 'comment should be at least 2 characters'] // Minimum 2 characters
     }
   },
   { versionKey: false }
