cbe9f7faa4679d87339102a0cabaca82b4358426
dayanjr <dayanfrazao@byui.edu>
2024-03-30 18:42:07 -0600

    up

--- schemas/ingredientSchema.js
+++ schemas/ingredientSchema.js
@@ -6,18 +6,24 @@ const IngredientSchema = new Schema(
   {
     name: {
       type: String,
-      required: true
+      required: [true, 'Name is required'],
+      maxlength: [200, 'Name cannot be more than 200 characters'] // Maximum 200 characters
+      
     },
     quantity: {
       type: Number,
-      required: true
+      required: [true, 'Quantity is required'],
     },
     unit: {
       type: String,
-      required: true
+      required: [true, 'Unit is required'],
+      maxlength: [200, 'Unit cannot be more than 200 characters'], // Maximum 200 characters
+      minlength: [2, 'Unit should be at least 2 characters'] // Minimum 2 characters
     },
     notes: {
-      type: String
+      type: String,
+      maxlength: [2000, 'Notes cannot be more than 200 characters'], // Maximum 200 characters
+      minlength: [2, 'Notes should be at least 2 characters'] // Minimum 2 characters
     }
   },
   { versionKey: false }
