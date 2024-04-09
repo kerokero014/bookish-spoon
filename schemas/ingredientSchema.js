9ec0ea77f96db6552025a8ded71598349695ffea
kerokero014 <kevinjrm.kr@gmail.com>
2024-04-09 17:06:42 -0600

    Update ingredient REST API and schema to support creating a new ingredient with name "Ripe mango", quantity 2 cups, unit "cups", and notes "peeled and chopped"

--- schemas/ingredientSchema.js
+++ schemas/ingredientSchema.js
@@ -10,18 +10,16 @@ const IngredientSchema = new Schema(
       maxlength: [200, 'Name cannot be more than 200 characters'] // Maximum 200 characters
     },
     quantity: {
-      type: Number,
-      required: [true, 'Quantity is required']
+      type: Number
     },
     unit: {
       type: String,
-      required: [true, 'Unit is required'],
       maxlength: [200, 'Unit cannot be more than 200 characters'], // Maximum 200 characters
       minlength: [2, 'Unit should be at least 2 characters'] // Minimum 2 characters
     },
     notes: {
       type: String,
-      maxlength: [2000, 'Notes cannot be more than 200 characters'], // Maximum 200 characters
+      maxlength: [200, 'Notes cannot be more than 200 characters'], // Maximum 200 characters
       minlength: [2, 'Notes should be at least 2 characters'] // Minimum 2 characters
     }
   },
